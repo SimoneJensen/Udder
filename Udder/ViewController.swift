6bfb67e38f3666feeac10e2ead3e51a0b9febd8a
SimoneJensen <simone.jensen@hotmail.co.uk>
2020-07-11 08:37:02 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Simone Jensen on 11/07/2020.
+//  Copyright © 2020 Simone Jensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
